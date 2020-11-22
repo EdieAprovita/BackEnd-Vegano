1ad51e9f98d7878dab2ee508f549f1064aaa05e6
EdieAprovita <edieveg316@gmail.com>
2020-11-21 19:10:38 -0600

    Fixing the auth controllers

--- BackEnd-Vegano-API/controllers/userControllers.js
+++ BackEnd-Vegano-API/controllers/userControllers.js
@@ -6,22 +6,21 @@ const User = require('../models/User')
 // @route   POST /api/users/login
 // @access  Public
 exports.authUser = asyncHandler(async (req, res) => {
-	try {
-		const { email, password } = req.body
+	const { email, password } = req.body
 
-		const user = await User.findOne({ email })
+	const user = await User.findOne({ email })
 
-		if (user && (await user.matchPassword(password))) {
-			res.status(200).json({
-				_id: user._id,
-				name: user.name,
-				email: user.email,
-				isAdmin: user.isAdmin,
-				token: generateToken(user._id),
-			})
-		}
-	} catch (error) {
-		res.status(400).json({ message: `${error}` })
+	if (user && (await user.matchPassword(password))) {
+		res.status(200).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		})
+	} else {
+		res.status(401)
+		throw new Error('Invalid email or password')
 	}
 })
 
